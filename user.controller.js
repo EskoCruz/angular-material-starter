6c0e56b2fd6dc063dd9209fb03eefb3215592017
Esko Cruzeta <eskocruz@users.noreply.github.com>
2017-07-08 23:50:41 -0700

    Bottom Sheet with Share Contacts

--- user.controller.js
+++ user.controller.js
@@ -43,9 +43,32 @@
              */
             function selectUser ( user ) {
                 vm.selected =  user;
+                vm.selected =  angular.isNumber(user) ? $scope.users[user] : user;
             }
+
+            ////////////////
             
-            function share(selectUser) {
+            function share(selectedUser) {
+
+                $mdBottomSheet.show({
+                    controller: UserSheetController,
+                    controllerAs: 'vm',
+                    templateUrl: './bottomsheet.html',
+                    parent: angular.element(document.querySelector('#content'))
+                });
+
+                function UserSheetController() {
+                    this.user = selectedUser;
+                    this.items = [
+                        {name: 'Phone', icon: 'phone', icon_url: 'svg/phone.svg'},
+                        {name: 'Twitter', icon: 'twitter', icon_url: 'svg/twitter.svg'},
+                        {name: 'Google+', icon: 'google_plus', icon_url: 'svg/google_plus.svg'},
+                        {name: 'Hangout', icon: 'hangouts', icon_url: 'svg/hangouts.svg'}
+                    ];
+                    this.performAction = function (action) {
+                        $mdBottomSheet.hide();
+                    };
+                }
 
             }
             
